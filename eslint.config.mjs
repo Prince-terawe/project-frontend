6cb55557f5950cdb2392ed9216c02a0fd1afa85d
Prince-terawe <princek@terawe.com>
2025-01-21 11:56:20 +0530

    test commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
@@ -5,6 +6,7 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
