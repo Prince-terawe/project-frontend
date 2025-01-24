031d73c576030cca7a4d0af6db2e8f1d4ad72dcb
Prince-terawe <princek@terawe.com>
2025-01-24 16:41:09 +0530

    sign-up page done

---
+++ src/Content/signUp.js
@@ -0,0 +1,16 @@
+const signUpContent = {
+  title: 'Sign In',
+  labels: {
+    name: 'Name',
+    email: 'Email',
+    password: 'Password',
+  },
+  placeHolders: {
+    name: 'Enter your name',
+    email: 'Enter your email',
+    password: 'Enter your password',
+  },
+  buttonText: 'Sign Up',
+};
+
+export default signUpContent;
