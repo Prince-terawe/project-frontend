6b510fd33f7bf40b702fc550c47038d538d9ee58
Prince-terawe <princek@terawe.com>
2025-02-19 16:07:52 +0530

    userlog detail show in profile

--- src/Content/profile.js
+++ src/Content/profile.js
@@ -1,6 +1,13 @@
 const ProfileContent = {
   title: 'Profile',
   noUser: 'No user data available.',
+  name: 'Name:',
+  email: 'Email:',
+  loginCount: 'Total Login:',
+  lastLogin: 'Last Login at:',
+  sessionExp: 'Session Expire at:',
+  duration: 'Time spend on Platform:',
+  timeMin: 'minutes',
 };
 
 export default ProfileContent;
